cf1669fba4beed20f4a44e9298198d99c1d27f26
origin <https://adams_zago@bitbucket.org/adams_zago/unaandroid.git>
2018-03-21 20:06:21 -0300

    atualizacao

---
+++ app/src/main/java/mobile/una/br/sendmessage/MainActivity.java
@@ -0,0 +1,41 @@
+package mobile.una.br.sendmessage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_inicial, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_explic:
+                startActivity(new Intent(this, SenderActivity.class));
+                return true;
+            case R.id.menu_implic:
+                startActivity(new Intent(this, SendImplicityActivity.class));
+                return true;
+            case R.id.menu_lifecycle:
+                startActivity(new Intent(this, LifecycleActivity.class));
+                return true;
+            case R.id.menu_sair:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
